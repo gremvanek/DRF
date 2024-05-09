9f317be683c346b40278d188071b9cd76df4ad0f
gremvanek <gremvanek@gmail.com>
2024-05-09 22:23:19 +0500

    Добавление модели Платежи по заданию 2.

--- users/models.py
+++ users/models.py
@@ -1,6 +1,8 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
+from course.models import Course, Lesson
+
 NULLABLE = {'null': True, 'blank': True}
 
 
@@ -18,3 +20,21 @@ class User(AbstractUser):
     class Meta:
         verbose_name = 'Пользователь'
         verbose_name_plural = 'Пользователи'
+        ordering = ('pk',)
+
+
+class Payment(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='оплативший пользователь')
+    payment_date = models.DateField(auto_now_add=True, verbose_name='дата оплаты')
+    course = models.ForeignKey(Course, on_delete=models.CASCADE, related_name='оплаченный курс', **NULLABLE)
+    lesson = models.ForeignKey(Lesson, on_delete=models.CASCADE, related_name='оплаченный урок', **NULLABLE)
+    payment_sum = models.PositiveIntegerField(verbose_name='сумма оплаты')
+    payment_method = models.CharField(choices=[('1', 'Наличные'), ('2', 'Перевод')], verbose_name='способ оплаты')
+
+    def __str__(self):
+        return f"{self.user}: {self.course if self.course else self.lesson} - {self.payment_date}"
+
+    class Meta:
+        verbose_name = 'платеж'
+        verbose_name_plural = 'платежи'
+        ordering = ('-payment_date',)
