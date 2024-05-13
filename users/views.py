aff1400f0e8bacd78afd23befe7576f0a5bd1e3d
gremvanek <gremvanek@gmail.com>
2024-05-13 19:29:40 +0500

    Методы создания и присваивании прав доступа.

--- users/views.py
+++ users/views.py
@@ -1,5 +1,7 @@
 from django_filters.rest_framework import DjangoFilterBackend
-from rest_framework import viewsets, generics
+from rest_framework import viewsets, generics, status
+from rest_framework.permissions import AllowAny
+from rest_framework.response import Response
 
 from users.models import User, Payment
 from users.serializers import UserSerializer, PaymentSerializer, UserProfileSerializer
@@ -10,12 +12,31 @@ class UserViewSet(viewsets.ModelViewSet):
     serializer_class = UserSerializer
     queryset = User.objects.all()
 
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+        u = User.objects.get(email=serializer.data['email'])
+        u.set_password(serializer.data['password'])
+        u.save()
+        headers = self.get_success_headers(serializer.data)
+        return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
+
+    def get_permissions(self):
+        if self.action in ['create']:
+            permission_classes = [AllowAny()]
+
+        else:
+            permission_classes = super().get_permissions()
+
+        return permission_classes
+
 
 class PaymentListAPIView(generics.ListAPIView):
     serializer_class = PaymentSerializer
     queryset = Payment.objects.all()
     filter_backends = [DjangoFilterBackend, OrderingFilter]
-    filter_set_fields = ('course', 'lesson', 'payment_method',)
+    filterset_fields = ('course', 'lesson', 'payment_method',)
     ordering_fields = ('payment_date',)
 
 
